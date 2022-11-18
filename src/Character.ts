f1127bc99b029cd63df8d7ab447f5efa0ae5f18b
celsorodrigo <celsorodrigoma@gmail.com>
2022-11-18 19:19:55 -0300

    Concluído 9° requisito.

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -37,9 +37,9 @@ export default class Character implements Fighter {
     return { type_: this._energy.type_, amount: this._energy.amount };
   }
 
-  attack(enemy:Fighter): void { enemy.receiveDamage(this.strength); }
+  attack(enemy:SimpleFighter): void { enemy.receiveDamage(this.strength); }
 
-  special?(enemy:Fighter): void {
+  special?(enemy:SimpleFighter): void {
     let critMultiplier = 1;
     const randomizer = getRandomInt(1, 20);
     if (randomizer <= 10) critMultiplier = 0;
